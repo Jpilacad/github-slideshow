97300a78d3781fcdc1f0f9829f519e4aa985bcd2
Jpilacad <74972204+Jpilacad@users.noreply.github.com>
2020-11-24 22:44:27 +0800

    Add files via upload

---
+++ aaa.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main(){
+clrscr();
+char str[100];
+printf("Enter your name");
+gets(str);
+printf("name of an employee:");
+puts(str);
+
+return 0;
+}
